12a953eef0aeb1ed259f8e63742f4966307701fd
Claudia <claudiatohyian@hotmail.com>
2016-07-12 16:00:52 +0800

    Alarm for 15secs button added. Snooze in background/foreground. App demonstrates where its launched from.

--- PushyApp/ViewController.m
+++ PushyApp/ViewController.m
@@ -9,10 +9,34 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+- (void) requestPermission;
+- (void) createNotification: (int) notificationDuration;
 @end
 
 @implementation ViewController
+
+//Implementation of app initiated notification.
+- (void) requestPermission{
+    UIUserNotificationType types = UIUserNotificationTypeAlert | UIUserNotificationTypeSound | UIUserNotificationTypeBadge;
+    UIUserNotificationSettings *settings= [UIUserNotificationSettings settingsForTypes:(types) categories:nil];
+    [[UIApplication sharedApplication]registerUserNotificationSettings:settings];
+}
+
+- (void) createNotification:(int)notificationDuration{
+    UILocalNotification *localNotification = [[UILocalNotification alloc]init];
+    localNotification.fireDate = [[NSDate date] dateByAddingTimeInterval:notificationDuration];
+    localNotification.timeZone = nil; //time zone follows user unless specified.
+    localNotification.alertTitle = @"Alert title.";
+    localNotification.alertBody = @"Alert body.";
+    localNotification.alertAction = @"Alert action.";
+    localNotification.soundName = UILocalNotificationDefaultSoundName; //using default sound alert.
+    localNotification.applicationIconBadgeNumber = 2929;
+    [[UIApplication sharedApplication] scheduleLocalNotification: localNotification];
+}
+- (IBAction)notifyUser:(id)sender {
+    [self requestPermission];
+    [self createNotification:15];//seconds after button is tapped
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
